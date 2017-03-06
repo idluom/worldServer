16f85a9b9289ac486cc12b2697be064161f2d579
fdhia <fdhia025@gmail.com>
2017-03-07 00:16:25 +0100

    dhia

--- skiworld/skiworld-ejb/src/main/java/Entity/Member.java
+++ skiworld/skiworld-ejb/src/main/java/Entity/Member.java
@@ -1,5 +1,117 @@
 package Entity;
 
-public class Member {
-	private int id;
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.Table;
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="member")
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+public class Member implements Serializable{
+	
+	private Integer id;
+	private String lastName;
+	private String firstName;
+	private String address;
+	private Integer cin;
+	private Integer numTel;
+	private Date birthDate;	
+	private String role;
+	private String login;
+	private String password;
+	private static final long serialVersionUID = 1L;
+	
+	public void member(){}
+
+	@Id  
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Integer getCin() {
+		return cin;
+	}
+
+	public void setCin(Integer cin) {
+		this.cin = cin;
+	}
+
+	public Integer getNumTel() {
+		return numTel;
+	}
+
+	public void setNumTel(Integer numTel) {
+		this.numTel = numTel;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+	
 }
