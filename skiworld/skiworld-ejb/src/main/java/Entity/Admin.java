b114b5a19fd06fdd6b89ec2ca36495a6257a3b5a
idluom <mohamedmouldi.slama@esprit.tn>
2017-03-07 01:32:48 +0100

    Push4

--- skiworld/skiworld-ejb/src/main/java/Entity/Admin.java
+++ skiworld/skiworld-ejb/src/main/java/Entity/Admin.java
@@ -6,8 +6,6 @@ import java.io.Serializable;
 import javax.persistence.*;
 
 @Entity
-@Table(name="admin")
-@DiscriminatorValue(value="admin")
 public class Admin extends Member implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
